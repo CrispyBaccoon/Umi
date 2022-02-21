4b46aa65e57ca9d872c4b1ed18875541563b250e
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2022-02-21 18:06:40 +0100

    not working.

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,20 +1,33 @@
-import React, { useCallback, useState } from "react";
+import React, { Dispatch, SetStateAction, useCallback, useState } from "react";
 import Head from "next/head";
 import Editor from "../components/Editor";
 import Preview from "../components/preview";
+import useDocument from "../components/useDocument";
 
 function App() {
-  const [doc, setDoc] = useState<string>("# hello world");
+  let [document, setDocument]:
+    | [string, Dispatch<SetStateAction<string>>]
+    | ["", () => void] = [null, null];
+  useDocument("D:/home/kitchen/documents/document.md").then(
+    (useDocumentHook) => {
+      [document, setDocument] = useDocumentHook;
+    }
+  );
 
-  const handleDocChange = useCallback((newDoc) => setDoc(newDoc), []);
+  // const [document, setDocument] = useState<string>(document);
+
+  const handleDocChange = useCallback(
+    (newDoc) => (setDocument == null ? setDocument(newDoc) : (n) => n),
+    [setDocument]
+  );
   return (
     <React.Fragment>
       <Head>
         <title>Umi</title>
       </Head>
       <div className="w-full h-full App">
-        <Editor onChange={handleDocChange} initialDoc={doc} />
-        <Preview doc={doc} />
+        <Editor onChange={handleDocChange} initialDoc={document} />
+        <Preview doc={document} />
       </div>
     </React.Fragment>
   );
